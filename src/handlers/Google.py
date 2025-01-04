b9be0b9bd827b0abdbffde92743e4ca8c5cc77a6
Leonel Marcos <mfjleonel@gmail.com>
2025-01-04 08:06:54 -0300

    feat: add argument parsing: domain, timeout, max_pages

--- src/handlers/Google.py
+++ src/handlers/Google.py
@@ -7,39 +7,53 @@ import time
 
 
 class Google(Handler):
-    def __init__(self, domain, offset = 1000):
+    def __init__(self, args):
         super().__init__()
-        self.domain = domain
-        self.offset = offset
-    
+        self.domain = args.domain
+        self.timeout = args.timeout
+        self.max_pages = args.max_pages
+        self.base_url = "https://www.google.com"
+
     def find_subdomains(self):
-        total_pages = self.get_total_pages()
+        total_pages = self.get_total_pages() if not self.max_pages else self.max_pages
 
         for page in range(0, total_pages):
-            time.sleep(2)
+            time.sleep(self.timeout)
             page = self.get_parsed_page(page)
             self.get_page_urls(page)
 
         self.show_results()
 
-
-
     def get_total_pages(self):
-        r = requests.get(f"https://www.google.com/search?q=site:*.{self.domain}&num=1", headers={'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0'})
-        soup = BeautifulSoup(r.content, 'html.parser')
-        results = soup.find("div", id='result-stats').get_text().replace(".", "").replace(",", "")
+        r = requests.get(
+            f"{self.base_url}/search?q=site:*.{self.domain}&num=1",
+            headers={
+                "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0"
+            },
+        )
+        soup = BeautifulSoup(r.content, "html.parser")
+        results = (
+            soup.find("div", id="result-stats")
+            .get_text()
+            .replace(".", "")
+            .replace(",", "")
+        )
         num_results = re.search(r"\d+", results)
         total_pages = math.floor(int(num_results.group(0)) / self.offset)
 
         return total_pages
 
-    def get_parsed_page(self, page = 1):
-        print(page * self.offset)
-        r = requests.get(f"https://www.google.com/search?q=site:*.{self.domain}&start={page * self.offset}&num={self.offset}", headers={'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0'})
-        soup = BeautifulSoup(r.content, 'html.parser')
+    def get_parsed_page(self, page=1):
+        r = requests.get(
+            f"{self.base_url}/search?q=site:*.{self.domain}&start={page * self.offset}&num={self.offset}",
+            headers={
+                "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0"
+            },
+        )
+        soup = BeautifulSoup(r.content, "html.parser")
 
         return soup
-    
+
     def get_page_urls(self, page):
         a_elements = page.find_all("a")
         urls = [u.get("href") for u in a_elements if isinstance(u.get("href"), str)]
@@ -54,6 +68,6 @@ class Google(Handler):
 
             if self.domain not in match:
                 continue
-            
+
             if match not in self.subdomains:
                 self.subdomains.append(match)
