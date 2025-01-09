56b674f19c852a03f323ba3bf3e476bd80afb046
Leonel Marcos <mfjleonel@gmail.com>
2025-01-09 09:24:27 -0300

    feat: add verbosity
    fix: Pipfile

--- src/handlers/Abstract.py
+++ src/handlers/Abstract.py
@@ -1,14 +1,18 @@
 from abc import ABC
 
+
 class Handler(ABC):
-    def __init__(self):
-        self.url_regex = r'https?://[a-zA-Z0-9.-]+'
+    def __init__(self, args):
+        self.url_regex = r"https?://[a-zA-Z0-9.-]+"
         self.subdomains = []
-        self.domain = ""
+        self.domain = args.domain
+        self.verbosity = args.v
+        self.timeout = int(args.timeout)
+        self.max_pages = args.max_pages
 
     def show_banner(self):
-        print('''Lazy o'subdomain''')
-    
+        print("""Lazy o'subdomain""")
+
     @classmethod
     def find_subdomains(self):
         pass
@@ -20,13 +24,17 @@ class Handler(ABC):
     @classmethod
     def get_parsed_page(self, page):
         pass
-    
+
     @classmethod
     def get_page_urls(self, page):
         pass
+
+    def log(self, message, verbosity_level=1):
+        if self.verbosity >= verbosity_level:
+            print(message)
 
     def show_results(self):
         print("\033[93m" + f"Unique subdomains found: {len(self.subdomains)}\n")
 
         for s in self.subdomains:
-            print('\033[92m' + s)
+            print("\033[92m" + s)
--- src/handlers/Google.py
+++ src/handlers/Google.py
@@ -8,15 +8,17 @@ import time
 
 class Google(Handler):
     def __init__(self, args):
-        super().__init__()
-        self.domain = args.domain
-        self.timeout = int(args.timeout)
-        self.max_pages = args.max_pages
-        self.offset = 1000
+        super().__init__(args)
+
+        self.offset = 100
         self.base_url = "https://www.google.com"
 
     def find_subdomains(self):
+        self.log("Searching for subdomains in Google", 0)
+
         total_pages = self.get_total_pages() if not self.max_pages else self.max_pages
+
+        self.log(f"Going through {total_pages} pages")
 
         for page in range(0, total_pages):
             time.sleep(self.timeout)
@@ -26,12 +28,15 @@ class Google(Handler):
         self.show_results()
 
     def get_total_pages(self):
+        self.log("Getting total number of pages to go through", 2)
+
         r = requests.get(
             f"{self.base_url}/search?q=site:*.{self.domain}&num=1",
             headers={
                 "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0"
             },
         )
+
         soup = BeautifulSoup(r.content, "lxml")
         results = (
             soup.find("div", id="result-stats")
@@ -42,6 +47,8 @@ class Google(Handler):
         num_results = re.search(r"\d+", results)
         total_pages = math.floor(int(num_results.group(0)) / self.offset)
 
+        self.log(f"Going through {total_pages} pages")
+
         return total_pages
 
     def get_parsed_page(self, page=1):
@@ -51,6 +58,9 @@ class Google(Handler):
                 "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0"
             },
         )
+
+        self.log(f"Page {page} responded with {r.status_code}", 2)
+
         soup = BeautifulSoup(r.content, "lxml")
 
         return soup
